0063e0b929f8fc00db6f1cfc5f16ee7c73d3f0e5
JEONGSUJI <wtw9611@gmail.com>
2020-01-13 11:11:07 +0900

    초기환경 세팅 및 기본모델 구성

---
+++ app/posts/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from members.models import User
+
+
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField(blank=True)
+    like_users = models.ManyToManyField(User, through='PostLike', related_name='like_posts_set')
+    created = models.DateTimeField(auto_now_add=True)
+
+
+class PostImage(models.Model):
+    post = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField()
+
+
+class PostComment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+
+
+class PostLike(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
