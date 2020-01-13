3008e61a4730c09bb3cf73d8dc154922ed013dd2
JEONGSUJI <wtw9611@gmail.com>
2020-01-13 11:18:28 +0900

    Post관련 목록 관리자에 추가 및 관리자 계정 생성 후 확인

--- app/posts/admin.py
+++ app/posts/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
 
-# Register your models here.
+from posts.models import Post, PostImage, PostComment, PostLike
+
+
+@admin.register(Post)
+class Post(admin.ModelAdmin):
+    pass
+
+@admin.register(PostImage)
+class PostImage(admin.ModelAdmin):
+    pass
+
+@admin.register(PostComment)
+class PostComment(admin.ModelAdmin):
+    pass
+
+@admin.register(PostLike)
+class PostLike(admin.ModelAdmin):
+    pass
